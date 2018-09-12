38cdb56a544e1647d68a6dc497a3678c7920fdb3
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-09-12 06:46:32 +0200

    Define isnumericval and isval

--- src/Tapl/Untyped/Arithmetic.elm
+++ src/Tapl/Untyped/Arithmetic.elm
@@ -17,3 +17,39 @@ type Term
     | TmSucc Term
     | TmPred Term
     | TmIsZero Term
+
+
+{-| Determine if a `Term` is a numerical value.
+
+Used in the evalution relation.
+
+-}
+isnumericalval : Term -> Bool
+isnumericalval term =
+    case term of
+        TmZero ->
+            True
+
+        TmSucc subTerm ->
+            isnumericalval subTerm
+
+        _ ->
+            False
+
+
+{-| Determine if a `Term` is a value.
+
+Similar to `isnumericalval`.
+
+-}
+isval : Term -> Bool
+isval term =
+    case term of
+        TmTrue ->
+            True
+
+        TmFalse ->
+            True
+
+        _ as otherTerm ->
+            isnumericalval otherTerm
